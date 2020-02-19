b86d02e006faa532d6f05dd179dc05bcc0f662e3
Brennan <btoles@balanceinnovations.com>
2020-02-19 12:28:33 -0600

    feat: Additional commands

--- README.md
+++ README.md
@@ -1,65 +1,22 @@
-# commit-kanbanize-id README
+# Commit Work Item
+> Link your commits to your issue/project tracking provider!
 
-This is the README for your extension "commit-kanbanize-id". After writing up a brief description, we recommend including the following sections.
+## What is it?
+Commit Work Item adds additional menu options for Commit, Commit Staged, and Commit All (with amends) to the main Source Control tree.
 
-## Features
+This command will prompt you for a three part message that will resolve to the following format:
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+```
+feat: New thing!            // short commit, include semantic keywords (optional)
 
-For example if there is an image subfolder under your extension project workspace:
+The new thing does this.    // more descriptive details of the commit
 
-\!\[feature X\]\(images/feature-x.png\)
+#id 1010                    // the work item id corresponding to this commit
+```
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+## Screenshots
 
-## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+## Releases
+### v0.0.1
+- Support for `#id #{workItemId}` format -- will be configurable in future releases.
--- src/extension.ts
+++ src/extension.ts
@@ -6,48 +6,15 @@ const Git = require('simple-git');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commit-kanbanize-id" is now active!');
 	const git = Git(workspace.rootPath);
 
-
-	// commands.executeCommand('editor.action.codeAction', )
-
-	// const kbnScm = vscode.scm.createSourceControl('kbn', 'Kanbanize', vscode.Uri.parse(vscode.workspace.rootPath as string));
-	// const kbnChanges = kbnScm.createResourceGroup('changes', 'CHANGES');
-	// kbnChanges.resourceStates.push({
-	// 	resourceUri: vscode.Uri.
-	// })
-
-	// kbnScm.acceptInputCommand =
-	// const kbnIdInput = window.createInputBox();
-
-	// kbnIdInput.
-	// kbnIdInput.
-	// vscode.window.showInputBox({
-	// 	ignoreFocusOut: true,
-	// 	prompt: 'Kanbanize Card ID',
-		// placeHolder: '02022020',
-		// validateInput(value) {
-		// 	// Match0.
-
-		// 	if (/[0-9]*/.test(value)) {
-		// 		return null;
-		// 	}
-		// 	// Return error message
-		// 	return 'Please enter a valid integer.'
-		// }
-	// })
-	// .then(err => {
-	// 	if (err) {
-	// 		vscode.window.showErrorMessage(err);
-	// 	}
-	// })
-
 	const commit = (messageParts: any[]) => git.commit(messageParts);
+	const commitStaged = (messageParts: any[]) => git.commit(messageParts);
+	const commitStagedAmend = (messageParts: any[]) => git.commit(messageParts, undefined, { '--amend': null });
+	const commitAll = (messageParts: any[]) => git.commit(messageParts, undefined, { '-a': null });
+	const commitAllAmend = (messageParts: any[]) => git.commit(messageParts, undefined, { '-a': null, '--amend': null });
 
+	// @ts-ignore
 	const preCommit = command => {
 		// The code you place here will be executed every time your command is executed
 		git.revparse(['--abbrev-ref', 'HEAD'], (currentBranch: string) => {
@@ -56,21 +23,24 @@ export function activate(context: ExtensionContext) {
 				placeHolder: 'chore: Adding additional logging',
 				prompt: 'Commit Message'
 			})
+			// @ts-ignore
 			.then((commitMessage: string) => {
 				window.showInputBox({
 					ignoreFocusOut: true,
 					prompt: 'Commit Details'
 				})
+				// @ts-ignore
 				.then((commitDetails: string) => {
 					let kanbanizeId;
 					if (currentBranch) {
 						// @ts-ignore
 						const [, id] = currentBranch.match(/[^\/]*([0-9]*)/);
-						const kanbanizeId = id;
+						kanbanizeId = id;
 					}
 
 					window.showInputBox({
 						ignoreFocusOut: true,
+						placeHolder: '02022020',
 						value: kanbanizeId,
 						valueSelection: [0, 0],
 						prompt: 'Kanbanize Card ID',
@@ -82,10 +52,11 @@ export function activate(context: ExtensionContext) {
 						// 	return 'An invalid ID was .'
 						// }
 					})
-						.then((kbnId: string) => {
-							try {
-								const cardId = `#id ${kbnId}`;
-								command([commitMessage, commitDetails, cardId]);
+					// @ts-ignore
+					.then((kbnId: string) => {
+						try {
+							const cardId = `#id ${kbnId}`;
+							command([commitMessage, commitDetails, cardId]);
 							window.showInformationMessage(`Commit tagged with Kanbanize ID ${kbnId}`);
 						}
 						catch (e) {
@@ -99,11 +70,17 @@ export function activate(context: ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = commands.registerCommand('kbn-git.commit', () => {
-		preCommit(commit);
-	});
+	let commitDispose = commands.registerCommand('kbn-git.commit', () => preCommit(commit));
+	let commitStagedDispose = commands.registerCommand('kbn-git.commitStaged', () => preCommit(commitStaged));
+	let commitStagedAmendDispose = commands.registerCommand('kbn-git.commitStagedAmend', () => preCommit(commitStagedAmend));
+	let commitAllDispose = commands.registerCommand('kbn-git.commitAll', () => preCommit(commitAll));
+	let commitAllAmendDispose = commands.registerCommand('kbn-git.commitAllAmend', () => preCommit(commitAllAmend));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(commitDispose);
+	context.subscriptions.push(commitStagedDispose);
+	context.subscriptions.push(commitStagedAmendDispose);
+	context.subscriptions.push(commitAllDispose);
+	context.subscriptions.push(commitAllAmendDispose);
 }
 
 // this method is called when your extension is deactivated
